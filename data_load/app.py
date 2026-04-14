70e25df310f5435c9106084206ea7d1696ce0d7e
VolodymyrMolnar <redbag1337@gmail.com>
2026-04-14 21:44:39 +0300

    update

--- data_load/app.py
+++ data_load/app.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/lab_db")
-DATA_PATH = "/app/data/dataset.csv"
+DATA_PATH = "/app/data/raw/dataset.csv"
 
 
 def get_engine():
